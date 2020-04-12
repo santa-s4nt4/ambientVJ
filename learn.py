0621cd80e67b17e2c9d21ef8117a5e7c71ce779d
santa-sukitoku <santanaruse@gmail.com>
2020-04-12 17:17:01 +0900

    feat: learn.pyにモジュールをインポート

---
+++ learn.py
@@ -0,0 +1,10 @@
+import keras
+from keras.models import Model
+from keras.layers import Input, Dense, Dropout, Activation
+from keras.layers import Conv2D, GlobalAveragePooling2D
+from keras.layers import BatchNormalization, Add
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+def main():
+    pass
